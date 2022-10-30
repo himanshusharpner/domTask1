ce5eb36b965b7566c6484fd5cc1345ab999611c9
Himan1999 <cu.16bcs1194@gmail.com[B>
2022-10-30 16:56:02 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,31 @@
+// console.log(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+var title = document.getElementsByClassName('title');
+var main = document.getElementById('main');
+
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerHTML = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h1>Hello</h3>'
+// header.style.borderBottom = 'soild 3px blue';
+// header.style.color = '#00FF00';
+   console.log(title);
+  title[0].style.fontWeight = "bold";
+  title[0].style.color = "yellow";
+
+
