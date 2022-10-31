5d85c0816aa5025cee4ffee7f7a06816ff6c2efb
Himan1999 <cu.16bcs1194@gmail.com[B>
2022-10-31 08:10:39 +0530

    getElementByClass

--- dom.js
+++ dom.js
@@ -12,20 +12,39 @@
 // console.log(document.links);
 // console.log(document.images);
 
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-var title = document.getElementsByClassName('title');
-var main = document.getElementById('main');
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+// var title = document.getElementsByClassName('title');
+// var main = document.getElementById('main');
 
 // console.log(headerTitle);
 // headerTitle.textContent = 'Hello';
 // headerTitle.innerHTML = 'Goodbye';
 // console.log(headerTitle.innerText);
 // headerTitle.innerHTML = '<h1>Hello</h3>'
-// header.style.borderBottom = 'soild 3px blue';
-// header.style.color = '#00FF00';
-   console.log(title);
-  title[0].style.fontWeight = "bold";
-  title[0].style.color = "yellow";
+// headerTitle.style.borderBottom = 'soild 3px black';
+// headerTitle.style.color = '#00FF00';
+//   console.log(title);
+//   console.log(main);
+//   title[0].style.fontWeight = "bold";
+//   title[0].style.color = "green";
+
+//GetElementByClassName
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+//items[1].style.fontWeight = 'bold';
+//items[1].style.backgroundColor = 'yellow';
+// items[0].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+// items[3].style.fontWeight = 'bold';
+for(var i=0; i < items.length; i++){
+  items[i].style.backgroundColor = '#f4f4f4';
+  items[2].style.backgroundColor = 'green';
+  items[i].style.fontWeight = 'bold';
+}
+
 
 
