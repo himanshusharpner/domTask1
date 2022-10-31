b5500870ef1428616dd1320e809d5441d40e7c94
Himan1999 <cu.16bcs1194@gmail.com[B>
2022-10-31 08:49:12 +0530

    GetElementByTagName

--- dom.js
+++ dom.js
@@ -31,19 +31,33 @@
 
 //GetElementByClassName
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-//items[1].style.fontWeight = 'bold';
-//items[1].style.backgroundColor = 'yellow';
-// items[0].style.fontWeight = 'bold';
-// items[2].style.fontWeight = 'bold';
-// items[3].style.fontWeight = 'bold';
-for(var i=0; i < items.length; i++){
-  items[i].style.backgroundColor = '#f4f4f4';
-  items[2].style.backgroundColor = 'green';
-  items[i].style.fontWeight = 'bold';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// //items[1].style.fontWeight = 'bold';
+// //items[1].style.backgroundColor = 'yellow';
+// // items[0].style.fontWeight = 'bold';
+// // items[2].style.fontWeight = 'bold';
+// // items[3].style.fontWeight = 'bold';
+// for(var i=0; i < items.length; i++){
+//   items[i].style.backgroundColor = '#f4f4f4';
+//   items[2].style.backgroundColor = 'green';
+//   items[i].style.fontWeight = 'bold';
+// }
+
+
+//GETELEMENTSBYTAGNAME //
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+
+for(var i=0; i < li.length; i++){
+  li[i].style.backgroundColor = '#f4f4f4';
 }
 
 
